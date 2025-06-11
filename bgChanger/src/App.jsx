589eb62a21163b137ad45b121824fc0089c088f8
Prattay9 <prattaydas9@gmail.com>
2025-06-12 02:17:28 +0530

    react update

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div className="w-full h-screen ease-in-out duration-500 "
         style={{ backgroundColor: color }}
       >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="mainContainer fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 bg-white shadow-lg px-3 py-2 rounded-2xl">
           <button 
             className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg"
